6c0d8c34ed880b8c069b2c39694191e402ff27b8
Development Laptop <241807358+ccloverdev@users.noreply.github.com>
2026-05-29 23:03:17 +0300

    SSR for the detailed post page

--- apps/main/src/routes/postview.js
+++ apps/main/src/routes/postview.js
@@ -9,14 +9,29 @@ require('dotenv').config();
 router.get('/:username/post/:postid', async (req, res) => {
     const { postid } = req.params;
 
-    let [data] = (await db.execute('SELECT * from posts WHERE post_id = ?', [postid]))[0] || {};
+    let [data] = (await db.execute(`
+        SELECT p.*, u.username AS author_name, u.profile_pic AS avatar,
 
-    if (!data){
-        res.render('post', {"user": req.session.user, "post": "None"});
-    }else{
-        res.render('post', {"user": req.session.user, "post": data});
-    }
+        -- Check user interaction
+        IF(l.user_id IS NOT NULL, 1, 0) AS isLiked,
+        IF(b.user_id IS NOT NULL, 1, 0) AS isBookmarked
+
+        FROM posts p
+        INNER JOIN users u ON p.author_id = u.user_id
+
+        -- Left join likes
+        LEFT JOIN likes l ON p.post_id = l.post_id AND l.user_id = ?
     
+        -- Left join bookmarks
+        LEFT JOIN bookmarks b ON p.post_id = b.post_id AND b.user_id = ?
+
+
+        WHERE p.post_id = ?
+    `, [req.session.user?.id || -1, req.session.user?.id || -1, (isNaN(postid) ? -1 : postid)] ) )[0] || null;
+
+    if (!data) console.log('post not found')
+
+    return res.render('post', {"user": req.session.user, "post": data});
 });
 
 // Export routes
