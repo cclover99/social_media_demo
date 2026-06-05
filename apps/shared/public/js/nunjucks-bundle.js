09c3978a143ce416fbabd4f255b1a12a493f0b58
Development Laptop <241807358+ccloverdev@users.noreply.github.com>
2026-06-05 23:05:57 +0300

    Add nunjucks json filter to frontend

--- apps/shared/public/js/nunjucks-bundle.js
+++ apps/shared/public/js/nunjucks-bundle.js
@@ -3,7 +3,8 @@ import nunjucks from 'nunjucks';
 // Import this file only specifically
 import postCard from '/apps/main/src/views/partials/postCard.njk?raw';
 
-nunjucks.configure({ autoescape: true });
+const env = nunjucks.configure({ autoescape: true });
+env.addFilter('json', (str) => JSON.parse(str));
 
 window.nunjucks = nunjucks;
 window.postCard = postCard;
